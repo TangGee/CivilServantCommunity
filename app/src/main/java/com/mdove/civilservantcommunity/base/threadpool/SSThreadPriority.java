a7bfdfc57b5fe56f91b932a4157a2f8cc17a520b
zhaojing <zhaojing.mdove@bytedance.com>
2019-08-28 13:29:44 +0800

    feat:base

---
+++ app/src/main/java/com/mdove/civilservantcommunity/base/threadpool/SSThreadPriority.java
@@ -0,0 +1,11 @@
+package com.mdove.civilservantcommunity.base.threadpool;
+
+/**
+ * Created by MDove on 2019/3/25.
+ */
+public enum SSThreadPriority {
+    LOW,
+    NORMAL,
+    HIGH,
+    IMMEDIATE
+}
